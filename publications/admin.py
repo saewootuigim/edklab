3277813d5174a7a103e4852d2bdf7a8ee1ad17ac
Seungbum Koo <19325010+saewootuigim@users.noreply.github.com>
2024-07-15 22:49:57 -0500

    disclose syracuse

--- publications/admin.py
+++ publications/admin.py
@@ -3,6 +3,6 @@ from .models import Publication
 
 @admin.register(Publication)
 class PublicationAdmin(admin.ModelAdmin):
-    list_display = ('title', 'journal', 'year')
+    list_display = ('type', 'title', 'journal', 'year')
     search_fields = ('title', 'authors', 'journal')
     list_filter = ('year', 'journal')
--- publications/models.py
+++ publications/models.py
@@ -2,12 +2,18 @@ from django.db import models
 from django.core.validators import URLValidator
 
 class Publication(models.Model):
+    type = models.CharField(max_length=13, default="Journal",
+        choices={
+            "Journal":"Journal",
+            "Protocol":"Protocol",
+            "Patent":"Patent",
+            "Dissertation":"Dissertation"})
     title = models.CharField(max_length=999)
     authors = models.TextField(help_text="Comma separated list of authors")
+    year = models.PositiveIntegerField()
     journal = models.CharField(max_length=255)
-    year = models.IntegerField()
-    volume = models.IntegerField(blank=True, null=True)
-    issue = models.IntegerField(blank=True, null=True)
+    volume = models.PositiveIntegerField(blank=True, null=True)
+    issue = models.PositiveIntegerField(blank=True, null=True)
     pages = models.CharField(max_length=50, blank=True, null=True)
     doi = models.URLField(validators=[URLValidator()])
 
