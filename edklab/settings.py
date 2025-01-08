9029914ace7de377e50743c256afbc63bb8ffeeb
Seungbum Koo <19325010+saewootuigim@users.noreply.github.com>
2025-01-07 19:03:23 -0500

    hide secret key

--- edklab/settings.py
+++ edklab/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,13 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g($@i&_-yd+y5@3o--(5v4r+!@dez_+69d@ya7i6*ku&1i)ml!'
+# SECRET_KEY = 'django-insecure-g($@i&_-yd+y5@3o--(5v4r+!@dez_+69d@ya7i6*ku&1i)ml!'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+# ALLOWED_HOSTS = ['saewootuigim.pythonanywhere.com', 'localhost']
 
-ALLOWED_HOSTS = ['saewootuigim.pythonanywhere.com', 'localhost']
-
+DEBUG = False
+ALLOWED_HOSTS = ['www.ekimlab.com']
 
 # Application definition
 
